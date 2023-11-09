ef7503ed35100b461b97d6ff62cebb8a7930ec5f
Slava <110696118+sl7one@users.noreply.github.com>
2023-11-09 22:10:39 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 
+document.documentElement.style.setProperty(
+  "--vh",
+  `${window.innerHeight * 0.01}px`
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
