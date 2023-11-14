7de07f8ac8d1c5cb22f0b9071883167c1a2df0e2
Slava <110696118+sl7one@users.noreply.github.com>
2023-11-14 11:34:02 +0200

    upd

--- src/components/PurcasesList/PurcasesList.jsx
+++ src/components/PurcasesList/PurcasesList.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./purchases-list.scss";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Icons from "../Icons/Icons";
 import DialogModal from "../DialogModal/DialogModal";
+import { animationsHelper } from "../../utils/animationsHelper";
+import { Toast } from "../../context/toast-context";
+import { deletePurchase } from "../../store/purchasesActions";
+
+const init = { name: "", phone: "", _id: "" };
 
 export default function PurcasesList() {
   const purchasesData = useSelector((state) => state.purchases.purchases);
+  const [client, setClient] = useState(init);
+  const { dialogModal, editClient } = animationsHelper;
+  const dispatch = useDispatch();
+  const toast = useContext(Toast);
+
+  const confirmDeleteContact = (client) => {
+    setClient(client);
+    dialogModal.show();
+  };
+
+  const onClickYes = ({ _id }) => {
+    dispatch(
+      deletePurchase({
+        data: _id,
+        success: () => {
+          toast.success("Контакт успешно удален");
+          dialogModal.hide();
+          setClient(init);
+        },
+        failed: (message) => toast.error(message),
+      })
+    );
+  };
 
   return (
     <>
       <ul className="purchases-list">
         {purchasesData.map(({ client: { _id, name, phone } }, idx) => (
-          <li className="clients-item" key={_id}>
-            <span className="clients-item__number">{++idx + "."}</span>
+          <li className="purchases-item" key={_id}>
+            <span className="purchases-item__number">{++idx + "."}</span>
             <span>{name}</span>
             <span>{phone}</span>
             <button
@@ -23,7 +51,7 @@ export default function PurcasesList() {
             </button>
             <button
               type="button"
-              //   onClick={() => confirmDeleteContact({ _id, name, phone })}
+              onClick={() => confirmDeleteContact({ _id, name, phone })}
             >
               <Icons name="remove" />
             </button>
@@ -32,10 +60,10 @@ export default function PurcasesList() {
       </ul>
       <DialogModal
         title="Подтвердите удаление"
-        // onClickYes={() => onClickYes({ _id: client._id })}
+        onClickYes={() => onClickYes({ _id: client._id })}
       >
-        {/* <span>{client.name}</span>
-        <span>{client.phone}</span> */}
+        <span>{client.name}</span>
+        <span>{client.phone}</span>
       </DialogModal>
     </>
   );
